b713e04584aa7bcac73fb0edd42fa18af0551b83
Stephen O'Shea <soshea44@hotmail.com>
2018-10-10 09:16:18 +0100

    Transfer

--- CS1HW4.py
+++ CS1HW4.py
@@ -34,11 +34,8 @@ def trapezoid(interval, weights):
     return approx
     
 def traparray(a, b, nmax):
-    N = 4;
-    interval, weights = intervals(a, b, N)
     intapprox = []
-    intapprox.append(trapezoid(interval, weights))
-    for i in range(3, nmax):
+    for i in range(2, nmax):
         N = 2 ** i
         interval, weights = intervals(a, b, N)
         intapprox.append(trapezoid(interval, weights))
@@ -69,8 +66,42 @@ def main(a, b, nmax):
         nvalues[i-2] = 2 ** i
     plt.loglog(nvalues, relerrors, 'bo')
     plt.show()
+    
+    baseapprox, mac1approx, mac2approx = traparrayimp(a, b, nmax)
+    relerrorbase = relerrorcalc(baseapprox, a, b)
+    relerrormac1 = relerrorcalc(mac1approx, a, b)
+    relerrormac2 = relerrorcalc(mac2approx, a, b)
+    
+    plt.figure()
+    plt.loglog(nvalues, relerrorbase, 'bo')
+    plt.loglog(nvalues, relerrormac1, 'ro')
+    plt.loglog(nvalues, relerrormac2, 'go')
+#    plt.ylim(ymin = 1e-1, ymax = 1e0)
+#    plt.xlim(xmax = 1e1)
+    plt.show()
+    
+    
+def traparrayimp(a, b, nmax):
+    intapprox = []
+    mac1approx = []
+    mac2approx = []
+    for i in range(2, nmax):
+        N = 2 ** i
+        interval, weights = intervals(a, b, N)
+        initialapprox = trapezoid(interval, weights)
+        #add the first maclaurin expansion term to the approximation = (h^2 / 12) * (f'x1 - f'xN)
+        #where f' is the derivative of f, will use the analytic values here i.e (sin x)' = cos x
+        #and cos 0 = 1, cos pi = -1
+        macapprox1 = initialapprox + ( ( (calch(a, b, N) ** 2) / 12 ) * 2 ) 
+        #same again but for the second maclaurin expansion term as well
+        #(sin x)''' - cos x
+        macapprox2 = macapprox1 - ( ( (calch(a, b, N) ** 4) / 720 ) * (-2))
+        intapprox.append(initialapprox)
+        mac1approx.append(macapprox1)
+        mac2approx.append(macapprox2)
+    return intapprox, mac1approx, mac2approx
 
     
     
-main(0, 2 * math.pi, 15)
+main(0, math.pi, 25)
     
---
+++ CS1scipyPythonlab.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  9 12:49:37 2018
+
+@author: Stephen
+"""
+from scipy import constants
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize_scalar
+import scipy
+h = constants.Planck
+c = constants.speed_of_light
+kB = constants.Boltzmann
+
+def planck(wl, temp):
+    u1 = ((2 * h * (c ** 2)) / (wl ** 5))
+    u2 = (1 / (np.exp((h * c ) / ((wl * kB * temp))) - 1))
+    u3 = u1 * u2
+    return u3
+
+def planck2(wl, temp):
+    return -planck(wl, temp)
+
+def find_max_wl(temp):
+    fmin = scipy.optimize.minimize_scalar(planck2, bounds = (0.00000001, 0.000002), args = (temp), method = 'bounded')
+    return fmin.x, -fmin.fun
+
+
+def main():
+    wls = np.linspace(1e-8, 2e-6, 200)
+    Ts = np.linspace(3000, 8000, 6)
+
+    fig, ax = plt.subplots()
+    for temp in Ts:
+        ax.plot(wls, planck(wls, temp), label=r'$T='+str(int(temp))+'\,\mathrm{K}$')
+        ax.ticklabel_format(axis='both', style='sci', scilimits=(-3,3))
+        ax.set_ylabel('Spectral radiance (kJ)')
+        ax.set_xlabel('Wavelength (nm)')
+    #
+    #       
+        
+        
+    ax.legend()
+    plt.show()
+
+
+
+    x = find_max_wl(3000)
+    print(x)
+main()
+#wls = np.linspace(1e-8, 2e-6, 200)
+#Ts = np.linspace(3000, 8000, 6)
+#wmaxima = np.zeros(len(Ts))
